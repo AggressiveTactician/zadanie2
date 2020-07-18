2b4ae5aeb59c3c2e64c213a7ff3bd3dd689a83ad
Użytkownik <uytkownik@kursGIT.pl>
2020-07-18 11:24:29 +0200

    zad2

--- src/Main.java
+++ src/Main.java
@@ -21,17 +21,28 @@ public class Main {
 //        Twój program powinien wypisać „BMI optymalne” lub „BMI nieoptymalne”, zgodnie z powyższymi założeniami.
 //
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj wagę: ");
-        float waga = scanner.nextFloat();
-        System.out.println("Podaj wzrost: ");
-        float wzrost = scanner.nextInt()/100;
-        float BMI = waga/(wzrost*wzrost);
-        System.out.println("BMI wynosi: " + BMI);
-        if (BMI < 18.5 || BMI > 24.9) {
-            System.out.println("BMI nieoptymalne");
-        } else {
-            System.out.println("BMI optymalne");
-        }
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Podaj wagę: ");
+//        float waga = scanner.nextFloat();
+//        System.out.println("Podaj wzrost: ");
+//        float wzrost = scanner.nextInt()/100;
+//        float BMI = waga/(wzrost*wzrost);
+//        System.out.println("BMI wynosi: " + BMI);
+//        if (BMI < 18.5 || BMI > 24.9) {
+//            System.out.println("BMI nieoptymalne");
+//        } else {
+//            System.out.println("BMI optymalne");
+//        }
+//        ============================================================================================================================
+//        Napisz  program  służący  do  rozwiązywania  równania  kwadratowego.
+//            Program powinien pobrać trzy liczby całkowite (współczynniki równania kwadratowego a, b, c) i wyliczyć pierwiastki x1, x2 równania ax^2 + bx + c = 0
+//        Jeśli delta ∆ wyjdzie ujemna, wypisz „Delta ujemna” i  zakończ program.
+//        Potrzebne wzory:
+//        delta = b^2 - 4ac
+//            x1 = = -b - pierwiastek z delty/2a
+//            x2 = -b + pierwiastek z delty/2a
+
+
+
     }
 }
