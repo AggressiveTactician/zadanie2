b258ca3c5eae4e77f1ea46d7003b0f6660385cdd
Użytkownik <uytkownik@kursGIT.pl>
2020-07-18 11:02:20 +0200

    zad2

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Napisz program, który pobierze od użytkownika średnicę okręgu (zmienną typu float) i obliczy obwód tego okręgu.
+        // Do obliczeń przyjmij najpierw pi = 3.14, a następnie skorzystaj z wbudowanej klasy Math i znajdującej się tam
+        //stałej pi
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Podaj średnicę: ");
+//        float diameter = scanner.nextFloat();
+//        float perimeter = (float) (2 * (Math.PI) * (diameter/2));
+//        System.out.println(perimeter);
+//        ========================================================================================================================
+
+//        Napisz program, obliczający BMI (Body Mass Index) i sprawdzający czy jest ono w normie. Program powinien wczytywać od użytkownika
+//        dwie zmienne: wagę w kilogramach (typ float) oraz wzrost w centymetrach (typ int). BMI powinno zostać wyliczone zgodnie z poniższym
+//        wzorem: BMI = masa/wzrost^2
+//        Optymalny  zakres  BMI  wynosi  od  18.5  do  24.9,  wartości  mniejsze  lub  większe  są wartościami nieoptymalnymi.
+//        Twój program powinien wypisać „BMI optymalne” lub „BMI nieoptymalne”, zgodnie z powyższymi założeniami.
+//
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj wagę: ");
+        float waga = scanner.nextFloat();
+        System.out.println("Podaj wzrost: ");
+
+    }
+}
