dd8c0fcbab86ca746e6bd0ac1178b2c17756142b
Użytkownik <uytkownik@kursGIT.pl>
2020-07-18 12:31:32 +0200

    zad4 rozwiązane

--- src/Main.java
+++ src/Main.java
@@ -63,9 +63,21 @@ public class Main {
 //        każda  w  kolejnej  linijce,  z  następującymi zmianami:
 //        ● w miejscu liczb podzielnych przez 3, zamiast liczby program powinien wypisać „Pif”
 //        ● w miejscu liczb podzielnych przez 7, zamiast liczby program powinien wypisać „Paf”
-//        ● jeśli liczba jest podzielna zarówno przez 3, jak i 7, program powinien wypisać „Pif paf”
+//        ● jeśli liczba jest podzielna zarówno przez 3, jak i 7, program powinien wypisać „Pif, paf”
 
-
-
+        System.out.println("Podaj dodatnią liczbę całkowitą: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        for (int i = 1; i <= number; i++) {
+            if (i % 3 == 0 && i % 7 == 0) {
+                System.out.println("Pif");
+            } else if (i % 7 == 0) {
+                System.out.println("Paf");
+            } else if (i % 3 == 0) {
+                System.out.println("Pif, paf");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
