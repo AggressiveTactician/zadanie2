ac14773759423ea52482f69bbe7f1ab83126fe5f
Użytkownik <uytkownik@kursGIT.pl>
2020-07-18 11:26:14 +0200

    zad2

--- src/Main.java
+++ src/Main.java
@@ -38,9 +38,9 @@ public class Main {
 //            Program powinien pobrać trzy liczby całkowite (współczynniki równania kwadratowego a, b, c) i wyliczyć pierwiastki x1, x2 równania ax^2 + bx + c = 0
 //        Jeśli delta ∆ wyjdzie ujemna, wypisz „Delta ujemna” i  zakończ program.
 //        Potrzebne wzory:
-//        delta = b^2 - 4ac
-//            x1 = = -b - pierwiastek z delty/2a
-//            x2 = -b + pierwiastek z delty/2a
+//        ∆ = b^2 - 4ac
+//            x1 = = -b - pierwiastek ∆/2a
+//            x2 = -b + pierwiastek ∆/2a
 
 
 
